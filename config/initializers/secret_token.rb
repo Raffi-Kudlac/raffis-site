e7caf95ec94161b313ae67bedfca9596d90c2ab0
Raffi <raffi.kudlac@gmail.com>
2013-10-11 20:21:22 -0700

    Initiial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RaffisSite::Application.config.secret_key_base = 'b64e31c6259975e9df3a391e88baba3e08ece26709e708658dc37b68ebc3ee83434fc53cc547b18e00e790d8b04954ad5b4ed4ed1ee08ce742752636e2a65725'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RaffisSite::Application.config.session_store :cookie_store, key: '_raffis_site_session'
